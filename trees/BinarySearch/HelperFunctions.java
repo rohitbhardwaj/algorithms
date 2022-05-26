823a042fa987543f91175a048997a293e41d847e
jyotibh2016 <40836079+jyotibh2016@users.noreply.github.com>
2022-05-26 16:31:41 -0400

    Add files via upload

---
+++ trees/BinarySearch/HelperFunctions.java
@@ -0,0 +1,40 @@
+import java.util.ArrayList;
+
+public class HelperFunctions {
+    // traverse in order
+    public ArrayList<Integer> traverseInOrder(Node node, ArrayList<Integer> array) {
+        if (node.left != null) {
+            traverseInOrder(node.left, array);
+        }
+        array.add(node.value);
+        if (node.right != null) {
+            traverseInOrder(node.right, array);
+        }
+        return array;
+    }
+
+    // traverse pre order
+    public ArrayList<Integer> traversePreOrder(Node node, ArrayList<Integer> array) {
+        array.add(node.value);
+        if (node.left != null) {
+            traversePreOrder(node.left, array);
+        }
+        if (node.right != null) {
+            traversePreOrder(node.right, array);
+        }
+        return array;
+    }
+
+    // traverse post order
+    public ArrayList<Integer> traversePostOrder(Node node, ArrayList<Integer> array) {
+        if (node.left != null) {
+            traversePostOrder(node.left, array);
+        }
+        if (node.right != null) {
+            traversePostOrder(node.right, array);
+        }
+        array.add(node.value);
+        return array;
+    }
+
+}
